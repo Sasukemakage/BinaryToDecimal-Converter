3e47d273eb8140b5404f0abbd0010c75d8d33fdb
Sasukemakage <69686119+Sasukemakage@users.noreply.github.com>
2021-04-07 20:06:16 +0200

    Add files via upload

---
+++ BinToDecConverter.py
@@ -0,0 +1,15 @@
+def convert(l):
+    intNumber = 0
+    l.reverse()
+    if len(l) == 0:
+        return None
+    for i in range(len(l)):
+        if l[i] == 0:
+            nbr = 0
+        if l[i] == 1:
+            nbr = 2**i
+        intNumber += nbr
+    return intNumber
+
+print(convert([1,0,1,0,0,1,1]))
+print(convert([1,0,1,0]))
